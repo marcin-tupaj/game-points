443c71a6db297ae931dee42d2586033a0fc1358d
Marcin Tupaj <marcintupaj@gmail.com>
2023-05-21 19:35:27 +0200

    [TICKET-2137] Update jest test match setting

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   roots: ['src'],
+  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.test.tsx'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
